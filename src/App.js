23537f6d35d1667507f64d6dd802d84f5167e347
vaibhavwaghode <waghode.vaibhav@gmail.com>
2024-10-28 17:59:36 +0530

    task done

--- src/App.js
+++ src/App.js
@@ -1,23 +1,72 @@
-import logo from './logo.svg';
+import {useState} from 'react';
+
 import './App.css';
 
 function App() {
+  const [friendlist,setFriendlist] = useState([]);
+  const [newfriend,setNewfriend]=useState("");
+  const [Newage,setNewage] = useState("");
+
+  //now todolist become array of object
+  const handleFriend = (event) =>
+  {
+    setNewfriend(event.target.value);
+  }
+
+  const handleAge = (event) => {
+    setNewage(event.target.value)
+  }
+
+  const AddFriend = ()=>{
+    if (newfriend === "" || Newage === " ") return;
+    const previousId = friendlist.length === 0 ? 1 : (friendlist[friendlist.length-1].id + 1)
+    const friend = {
+      id: previousId  ,
+      friendName: newfriend,
+      age:Newage
+    }
+    setFriendlist([...friendlist,friend])
+    setNewfriend("")
+    setNewage("")
+  }
+
+  const deleteFriend = (id) =>
+  {
+    setFriendlist(friendlist.filter((friend)=>friend.id !== id));
+  }
+
+  const EditFriend = (id) =>{
+    const Newage = prompt("enter the new age")
+    if(Newage ===  "" || Newage.trim() === "") return;
+    setFriendlist(friendlist.map((friend)=>{
+      return friend.id === id ? {...friend,age:Newage} : friend;
+    }))
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className='top-container'>
+            <div className="Input-task">
+              <input onChange={handleFriend} value={newfriend} className='input-box input-aln' placeholder='Enter friend here...'/>
+              <input onChange={handleAge} value={Newage} className='input-box input-aln' placeholder='Enter age here...'/>
+
+              <button onClick={AddFriend} className='task-btn input-aln'>Add Friend</button>
+            </div>
+            <div className="list">
+            {friendlist.map((friend)=>{
+                return(
+                      <div className='todo-name'>
+                        <h1>Name:{friend.friendName}</h1>
+                        <h1>age:{friend.age}</h1>
+
+                        <button onClick={()=>EditFriend(friend.id)} className='task-edit'>Edit Friend</button>
+                        <button onClick={()=>deleteFriend(friend.id)} className='task-edit task-delete-btn' >X</button>
+                      </div>
+                      )
+                })}
+            </div>
+
+      </div>
+
     </div>
   );
 }
